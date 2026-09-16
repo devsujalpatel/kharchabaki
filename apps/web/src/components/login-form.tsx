03d645e576285b4484d006324b9846ce3779f871
Sujal Patel <sujal7455@gmail.com>
2026-09-17 01:08:32 +0530

    fixed cors issue

--- apps/web/src/components/login-form.tsx
+++ apps/web/src/components/login-form.tsx
@@ -19,7 +19,7 @@ export function LoginForm({
   const handleClick = async () => {
     await authClient.signIn.social({
       provider: "google",
-      callbackURL: `${process.env.NEXT_PUBLIC_WEB_URL}/dashboard`,
+      callbackURL: `${process.env.WEB_URL}/dashboard`,
     });
   };
   return (
