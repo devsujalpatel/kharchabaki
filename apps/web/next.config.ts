253848168eee6a595421c696b3b872603b0a901a
Sujal Patel <sujal7455@gmail.com>
2026-09-17 13:05:53 +0530

    fixed auth issue

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -3,14 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  async rewrites() {
-    return [
-      {
-        source: "/api/auth/:path*",
-        destination: "https://kharchabaki.onrender.com/api/auth/:path*"
-      }
-    ]
-  }
 };
 
 export default nextConfig;
