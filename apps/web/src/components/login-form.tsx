d6e1680d22819e99bcf58024b5032a7a27087aab
Sujal Patel <sujal7455@gmail.com>
2026-09-18 04:10:53 +0530

    changes in login form

--- apps/web/src/components/login-form.tsx
+++ apps/web/src/components/login-form.tsx
@@ -135,7 +135,7 @@ export function LoginForm({
                   onClick={() =>
                     authClient.signIn.social({
                       provider: "google",
-                      callbackURL: `${process.env.NEXT_PUBLIC_APP_UEL}/dashboard`,
+                      callbackURL: `${process.env.NEXT_PUBLIC_APP_URL}/dashboard`,
                     })
                   }
                 >
