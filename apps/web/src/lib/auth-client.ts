be726c4898149bfba61a3ce9d59c70449238a50d
Sujal Patel <sujal7455@gmail.com>
2026-09-20 18:58:46 +0530

    change in baseUrl

--- apps/web/src/lib/auth-client.ts
+++ apps/web/src/lib/auth-client.ts
@@ -2,7 +2,7 @@ import { adminClient } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  baseURL: "/api/auth",
   fetchOptions: {
     credentials: "include",
   },
